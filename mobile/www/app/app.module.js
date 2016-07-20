2ecdb99426a5d24705bd953ae62c7c40023ca3eb
Livio Brunner <livio.brunner.lb1@roche.com>
2016-07-20 11:26:37 +0200

    change folder structure

---
+++ mobile/www/app/app.module.js
@@ -0,0 +1,14 @@
+angular.module('pokemonGOPokedex', ['ionic'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
