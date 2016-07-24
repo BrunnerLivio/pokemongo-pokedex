9f99d09b5f2db99f65ebf644e6f3ac7789f36cbe
Livio Brunner <livio.brunner@hotmail.com>
2016-07-24 18:57:15 +0200

    clean up

--- mobile/gulpfile.js
+++ mobile/gulpfile.js
@@ -17,12 +17,12 @@ gulp.task('sass', function(done) {
   gulp.src('./scss/ionic.app.scss')
     .pipe(sass())
     .on('error', sass.logError)
-    .pipe(gulp.dest('./www/css/'))
+    .pipe(gulp.dest('./www/assets/css/'))
     .pipe(minifyCss({
       keepSpecialComments: 0
     }))
     .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./www/css/'))
+    .pipe(gulp.dest('./www/assets/css/'))
     .on('end', done);
 });
 
