cde317b74ccc786ab8e98f9a0ac9b36764f39cc4
Livio Brunner <livio.brunner.lb1@roche.com>
2016-07-26 17:17:16 +0200

    add detail view

--- mobile/gulpfile.js
+++ mobile/gulpfile.js
@@ -8,7 +8,8 @@ var rename = require('gulp-rename');
 var sh = require('shelljs');
 
 var paths = {
-  sass: ['./scss/**/*.scss']
+  sass: ['./scss/**/*.scss',
+        './www/**/*.scss']
 };
 
 gulp.task('default', ['sass']);
