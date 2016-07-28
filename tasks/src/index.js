256c1b3a8cdcfda1af4f67ccf99e5d5e77fda263
Livio Brunner <livio.brunner.lb1@roche.com>
2016-07-28 09:54:04 +0200

    add pokegodex cli

---
+++ tasks/src/index.js
@@ -0,0 +1,42 @@
+import program from 'commander';
+import fs from 'fs';
+import path from 'path';
+import _ from 'lodash';
+import childProcess from 'child_process';
+
+program
+    .version('0.0.1')
+    .option('-t, --task <s>', 'The name of the task')
+    .parse(process.argv);
+
+function runScript(scriptPath, callback) {
+
+    var invoked = false;
+
+    var process = childProcess.fork(scriptPath);
+    process.on('error', function (err) {
+        if (invoked) return;
+        invoked = true;
+        callback(err);
+    });
+
+    process.on('exit', function (code) {
+        if (invoked) return;
+        invoked = true;
+        var err = code === 0 ? null : new Error('exit code ' + code);
+        callback(err);
+    });
+
+}
+
+function getDirectories(srcpath) {
+    return fs.readdirSync(srcpath).filter(function (file) {
+        return fs.statSync(path.join(srcpath, file)).isDirectory();
+    });
+}
+let tasks = getDirectories(__dirname + '/tasks');
+if (_.includes(tasks, program.task)) {
+    runScript(__dirname + '/tasks/' + program.task + '/index.js', (err) => {
+        if (err) throw err;
+    });
+}
